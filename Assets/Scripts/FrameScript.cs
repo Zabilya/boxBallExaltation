1a9420e69840725c80b2b4674112564dd9873b2d
unknown <zaza.1395@mail.ru>
2020-06-17 12:37:49 +0300

    add frameController

---
+++ Assets/Scripts/FrameScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameScript : MonoBehaviour
+{
+    public float xAxis;
+    public float yAxis;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(xAxis / 2, yAxis / 2), ForceMode2D.Impulse);
+    }
+}
