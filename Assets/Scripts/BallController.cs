6afe6a834161d9c6c36133acf923997a6a6ce7e4
unknown <zaza.1395@mail.ru>
2020-06-17 11:50:08 +0300

    improve ball controller

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,31 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BallController : MonoBehaviour
 {
     public Vector2 direction;
-    public bool isStuck;
 
     // Start is called before the first frame update
     void Start()
     {
-        direction = new Vector2(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        //TODO рандомные стороны
+        direction = new Vector2(Random.Range(0.5f, 1.0f) * 2, Random.Range(0.5f, 1.0f) * 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isStuck)
-        {
-            direction = Vector2.Reflect(direction, Vector2.left);
-            isStuck = false;
-        }
+        CheckIsStuck();
         Move(direction);
     }
 
     private void Move(Vector2 dir)
     {
         transform.Translate(dir * Time.deltaTime);
+    }
+
+    private void CheckIsStuck()
+    {
+        Vector2 pos = transform.position;
+        if (pos.x >= 6.75)
+        {
+            CalculateReflectDir(Vector2.left);
+        }
+        else if (pos.x <= -6.75)
+        {
+            CalculateReflectDir(Vector2.right);
+        }
+        else if (pos.y >= 3.75)
+        {
+            CalculateReflectDir(Vector2.down);
+        }
+        else if (pos.y <= -3.75)
+        {
+            CalculateReflectDir(Vector2.up);
+        }
+    }
+
+    private void CalculateReflectDir(Vector2 norm)
+    {
+        direction = Vector2.Reflect(direction, norm);
     }
 }
